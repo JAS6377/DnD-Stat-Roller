8bdd01d15971c408dfe7838ea0450d6e48fc1208
Jason Smith <54783969+JAS6377@users.noreply.github.com>
2021-01-14 00:01:24 -0500

    Stat_Roller.py
    I realized that I missed the 6th stat. That's been fixed.

--- Stat_Roller.py
+++ Stat_Roller.py
@@ -1,10 +1,13 @@
 # Created by Jason Smith
 # September 2, 2019
+#
+# Updated January 13, 2021
+# Added sixth stat because D&D does indeed have six stats. Who would have thought?
 
 # Define Variables
 import random
 import math
-stats = 5
+stats = 6
 diesize = 6
 rolls = int(input("How many dice per stat? (3 for hardcore, 4 will drop the lowest from each set) "))
 fail = 0
@@ -79,6 +82,7 @@ stat2 = statoutput[rolls:rolls*2]
 stat3 = statoutput[rolls*2:rolls*3]
 stat4 = statoutput[rolls*3:rolls*4]
 stat5 = statoutput[rolls*4:rolls*5]
+stat6 = statoutput[rolls*5:rolls*6]
 
 if rolls == 4:
     stat1.remove(min(stat1))
@@ -86,18 +90,21 @@ if rolls == 4:
     stat3.remove(min(stat3))
     stat4.remove(min(stat4))
     stat5.remove(min(stat5))
+    stat6.remove(min(stat6))
 
 s1 = sum(stat1)
 s2 = sum(stat2)
 s3 = sum(stat3)
 s4 = sum(stat4)
 s5 = sum(stat5)
+s6 = sum(stat6)
 
 s1x = math.floor((s1-10)/2)
 s2x = math.floor((s2-10)/2)
 s3x = math.floor((s3-10)/2)
 s4x = math.floor((s4-10)/2)
 s5x = math.floor((s5-10)/2)
+s6x = math.floor((s6-10)/2)
 
 # Print the roll outputs
 print("Stat 1 rolls: ", stat1)
@@ -105,7 +112,7 @@ print("Stat 2 rolls: ", stat2)
 print("Stat 3 rolls: ", stat3)
 print("Stat 4 rolls: ", stat4)
 print("Stat 5 rolls: ", stat5)
-print()
+print("Stat 6 rolls: ", stat6)
 
 # Print the base stats and bonuses
 print("Stat 1 total is ", s1, ", and the bonus is +", s1x)
@@ -113,3 +120,4 @@ print("Stat 2 total is ", s2, ", and the bonus is +", s2x)
 print("Stat 3 total is ", s3, ", and the bonus is +", s3x)
 print("Stat 4 total is ", s4, ", and the bonus is +", s4x)
 print("Stat 5 total is ", s5, ", and the bonus is +", s5x)
+print("Stat 6 total is ", s6, ", and the bonus is +", s6x)
